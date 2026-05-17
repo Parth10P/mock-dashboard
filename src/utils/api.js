1e278999d357b1ca72462a8785bc7483d6e92358
Parth10P <patel.parthkumar@adypu.edu.in>
2026-05-17 18:50:17 +0530

    feat: add API utility with Axios integration

---
+++ src/utils/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const fetchProfile = (userId) =>
+  api.get("/api/auth/profile", {
+    headers: { "x-user-id": userId },
+  }).then((res) => res.data);
+
+export const fetchDashboard = (userId) =>
+  api.get("/api/auth/dashboard", {
+    headers: { "x-user-id": userId },
+  }).then((res) => res.data);
+
+export const fetchStats = (userId) =>
+  api.get("/api/call-sessions/stats", {
+    headers: { "x-user-id": userId },
+  }).then((res) => res.data);
+
+export const fetchCallHistory = (userId, limit = 10) =>
+  api.get(`/api/call-sessions?limit=${limit}`, {
+    headers: { "x-user-id": userId },
+  }).then((res) => res.data);
